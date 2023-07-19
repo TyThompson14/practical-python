34d5d9f0772ec82b2d601227a8ee5ff103d07718
Ty Thompson <tythompson14@protonmail.com>
2023-07-19 14:11:31 -0400

    working on chapter 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_height = 100
+bounces = 10
+bounce = 1
+
+while bounce <= bounces:
+    ball_height = ball_height * (3/5)
+    print(bounce, round(ball_height,4))
+    bounce = bounce + 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,25 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 1
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+
+
+while principal > 0:
+    cur_payment = payment
+    if month >= extra_payment_start_month or month <= extra_payment_end_month:
+        cur_payment = payment + extra_payment
+    principal = principal * (1+rate/12) - cur_payment
+    total_paid = total_paid + cur_payment
+    month = month + 1
+
+print('Total paid', total_paid)
+print('total months', month)
